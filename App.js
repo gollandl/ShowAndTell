2339aaf9ac3540ef79007898302ea3ae29d09b27
Elliot Hanger <hangere@hangere.local>
2017-06-26 13:47:45 +0100

    changed welcome to British gas, and login

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import OptionsScreen from './src/Components/OptionsScreen';
 
 class HomeScreen extends React.Component {
   static navigationOptions = {
-    title: 'Welcome',
+    title: 'British Gas',
   };
   render() {
     const { navigate } = this.props.navigation;
@@ -20,7 +20,7 @@ class HomeScreen extends React.Component {
       <View>
         <Button
           onPress={() => navigate('Options')}
-          title="Enter"
+          title="Login"
         />
       </View>
     );
