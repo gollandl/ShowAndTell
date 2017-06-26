03c12c2d3bee6abc433ab50fb2ae4e95a324b469
Elliot Hanger <elliot.hanger@britishgas.co.uk>
2017-06-26 15:05:46 +0100

    made home page into a login page

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ import {
   View,
   Button,
   Platform,
+  Image,
+  TextInput,
+  StyleSheet,
 } from 'react-native';
 import { StackNavigator, TabNavigator } from 'react-navigation';
 
@@ -17,7 +20,13 @@ class HomeScreen extends React.Component {
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View>
+      <View style={styles.container}>
+        <Image style={styles.pic} source={require('./images/bg.png')}/>
+        <Text style={styles.text}>Please login</Text>
+
+        <TextInput style={styles.textinput} placeholder={'Username'} placeholderTextColor={"rgba(198,198,204,1)"}/>
+        <TextInput style={styles.textinput} placeholder={'Password'} placeholderTextColor={"rgba(198,198,204,1)"}secureTextEntry ref={(input) => this.passwordInput = input}/>
+
         <Button
           onPress={() => navigate('Options')}
           title="Login"
@@ -31,5 +40,37 @@ const ReactNavApp = StackNavigator({
     Home: { screen: HomeScreen },
     Options: { screen: OptionsScreen },
 });
+
+
+const styles = StyleSheet.create({
+  pic: {
+    flexGrow:1,
+    width:350,
+    height:270,
+    alignSelf:'center',
+    flexBasis:'27%',
+    marginBottom:50,
+  },
+  textinput: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginLeft: 65,
+      height: 20,
+      width: 250,
+      borderWidth: 1,
+      borderColor: "rgba(0,0,0,0.5)"
+    },
+
+    text: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  },
+container:{
+  alignItems:'center',
+  justifyContent:'center',
+  marginTop:100,
+},
+})
 
 AppRegistry.registerComponent('ReactNavApp', () => ReactNavApp);
